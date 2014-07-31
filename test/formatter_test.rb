9c37a4a9cc268a4fac01015b90e4e5c10cc1aa47
Ian Vaughan <github@ianvaughan.co.uk>
2014-07-31 12:16:17 +0100

    use h1 tags

--- test/formatter_test.rb
+++ test/formatter_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 # TODO: The idea will be to require the user to provide this as a callback
 class FormatterTest < ActiveSupport::TestCase
-  test 'a format' do
-    assert_equal "test", MultipartErb::Formatter.to_html("test").strip
+  test 'formatted output' do
+    assert_equal "test", MultipartErb::Formatter.to_html("<h1>test</h1>").strip
   end
 end
