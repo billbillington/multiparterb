489c417e3b935e5a9b14140156801f32f150fe3b
Ian Vaughan <github@ianvaughan.co.uk>
2014-08-04 19:03:38 +0100

    Tidy method calls

--- test/formatters/my_html_formatter.rb
+++ test/formatters/my_html_formatter.rb
@@ -1,10 +1,10 @@
 class MyHTMLFormatter < MultipartErb::BaseFormatter
   def heading(text)
-    content_tag :h1, text
+    content_tag(:h1, text)
   end
 
   def text(text)
-    content_tag :p, text
+    content_tag(:p, text)
   end
 
   def anchor(text, href)
